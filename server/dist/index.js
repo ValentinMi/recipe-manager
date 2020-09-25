48cc5693544f76b41fb79827a4c0c3e9de309d48
Valentin Misiaszek <dev@misiaszek-valentin.com>
2020-09-25 17:27:54 +0200

    Add apollo, graphql, express, typeorm dependencies

--- server/dist/index.js
+++ server/dist/index.js
@@ -1,2 +1,58 @@
-console.log("Hello man");
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const apollo_server_express_1 = require("apollo-server-express");
+const type_graphql_1 = require("type-graphql");
+const dotenv = __importStar(require("dotenv"));
+const express_1 = __importDefault(require("express"));
+dotenv.config();
+const PORT = process.env.SERVER_PORT;
+const main = () => __awaiter(void 0, void 0, void 0, function* () {
+    const app = express_1.default();
+    const apolloServer = new apollo_server_express_1.ApolloServer({
+        schema: yield type_graphql_1.buildSchema({
+            resolvers: [],
+            validate: false
+        }),
+        context: ({ req, res }) => ({ req, res })
+    });
+    apolloServer.applyMiddleware({
+        app,
+        cors: false
+    });
+    app.listen(process.env.PORT, () => {
+        console.log(`Server is listening on port ${PORT}`);
+    });
+});
+main().catch(err => console.error(err));
 //# sourceMappingURL=index.js.map
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,37 @@
-console.log("Hello man");
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import * as dotenv from "dotenv";
+import express from "express";
+dotenv.config();
+const PORT = process.env.SERVER_PORT;
+
+const main = async () => {
+  //   const conn = await createConnection({
+  //     type: "postgres",
+  //     database: "recipeManager",
+  //     username: "postgres",
+  //     password:
+  //   });
+
+  const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [],
+      validate: false
+    }),
+    context: ({ req, res }) => ({ req, res })
+  });
+
+  apolloServer.applyMiddleware({
+    app,
+    cors: false
+  });
+
+  app.listen(process.env.PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+  });
+};
+
+// Handling server error
+main().catch(err => console.error(err));
