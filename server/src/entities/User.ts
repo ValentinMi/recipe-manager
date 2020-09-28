dd72b8292351719b46543f083823d84916114ff0
Valentin Misiaszek <dev@misiaszek-valentin.com>
2020-09-28 17:56:11 +0200

    feat(user): create user entity & user resolvers

---
+++ server/src/entities/User.ts
@@ -0,0 +1,50 @@
+import Joi from "joi";
+import { UsernamePasswordInput } from "src/resolvers/UsernamePasswordInput";
+import { ObjectType, Field } from "type-graphql";
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  Column,
+  BaseEntity
+} from "typeorm";
+
+@ObjectType()
+@Entity()
+export class User extends BaseEntity {
+  @Field()
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Field()
+  @Column({ unique: true })
+  username!: string;
+
+  @Field()
+  @Column({ unique: true })
+  email!: string;
+
+  @Column()
+  password!: string;
+
+  @Field(() => String)
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @Field(() => String)
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
+
+export const validateUser = (
+  user: UsernamePasswordInput
+): Joi.ValidationResult => {
+  const schema = Joi.object({
+    username: Joi.string().min(3).max(25).required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().min(7).max(1055)
+  });
+
+  return schema.validate(user);
+};
